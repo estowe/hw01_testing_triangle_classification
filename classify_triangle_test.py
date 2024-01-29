d847c39e3256abfe0f999e40823cb76b139bfa73
estowe <estowe@stevens.edu>
2024-01-29 12:30:26 -0700

    Added another test

--- classify_triangle_test.py
+++ classify_triangle_test.py
@@ -1,22 +1,24 @@
 import pytest
 from classify_triangle import ClassifyTriangle, InvalidTriangleError
 
-def test_right_triangle():
-    tst_classify_triangle = ClassifyTriangle(17, 15, 8)
+@pytest.mark.parametrize("side1, side2, side3", [(17,15,8), (3,4,5), (5,12,13)])
+def test_right_triangle(side1, side2, side3):
+    tst_classify_triangle = ClassifyTriangle(side1, side2, side3)
     is_right_triangle = tst_classify_triangle.right_triangle()
-    #additional parms 3, 4, 5; 5, 12, 13; 17,15,8
     assert is_right_triangle == True
 
 def test_not_right_triangle():
-    tst_classify_triangle = ClassifyTriangle(17, 15, 9)
+    tst_classify_triangle = ClassifyTriangle(4, 5, 6)
     is_not_right_triangle = tst_classify_triangle.right_triangle()
     assert is_not_right_triangle == False
 
+@pytest.mark.parametrize("side1, side2, side3", [(2,2,3), (2,3,2), (3,2,2)])
+def test_isosceles_triangle(side1, side2, side3):
+    tst_classify_triangle = ClassifyTriangle(side1, side2, side3)
+    is_isosceles_triangle = tst_classify_triangle.isosceles_triangle()
+    assert is_isosceles_triangle == True
+
 '''
-def test_scalene_triangle():
-
-def test_isosceles_triangle():
-
 def test_equilateral_triangle():
 
 def test_non_triangle_integers():
